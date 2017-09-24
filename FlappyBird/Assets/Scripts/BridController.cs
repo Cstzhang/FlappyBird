fce486551fc5481da58cac3d38b19631322bead5
Cstzhang <835066041@qq.com>
2017-09-24 21:54:13 +0800

    游戏状态，开始游戏

--- FlappyBird/Assets/Scripts/BridController.cs
+++ FlappyBird/Assets/Scripts/BridController.cs
@@ -8,41 +8,65 @@ public class BridController : MonoBehaviour {
     public int frameNumber = 15;
     //时间计数器 通过时间流逝判断当前是第几帧。
     public float timer;
-    // Use this for initialization
+    //是否有动画
+    public bool whetherAnimation = false;
+    //是否可以跳跃
+	public bool canJump = false;
+     // Use this for initialization
 	void Start () {
-		//初试速度
-		this.GetComponent<Rigidbody>().velocity = new Vector3(2, 0, 0);
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//向左速度
-		//Vector3 vel = this.GetComponent<Rigidbody>().velocity;
+        //向左速度
+        //Vector3 vel = this.GetComponent<Rigidbody>().velocity;
         //this.GetComponent<Rigidbody>().velocity = new Vector3(5, vel.y, vel.z);
         // aniamtion
-        timer += Time.deltaTime;
-        if(timer >= 1.0f / frameNumber){
-            frameCount++;//帧数加一
-            timer -= 1.0f / frameNumber;
-            //update material's offset x
-            int frameIndex = frameCount % 3;//"图片有3帧"
+        if(GameManager._intance.GameStatus == GameManager.GAMESTATUS_PLAYING){
+			timer += Time.deltaTime;
+			if (timer >= 1.0f / frameNumber)
+			{
+				frameCount++;//帧数加一
+				timer -= 1.0f / frameNumber;
+				//update material's offset x
+				int frameIndex = frameCount % 3;//"图片有3帧"
 
-			// how to set the property offset "_MainTex",new Vector2(0.33333f * frameIndex,0)
-            this.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(0.33333f * frameIndex, 0));
+				// how to set the property offset "_MainTex",new Vector2(0.33333f * frameIndex,0)
+				this.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(0.33333f * frameIndex, 0));
+			}
+
+
         }
 
-        //control jump
-        if (Input.GetMouseButton(0)) {//lett mause bt down
-            Vector3 vel2 = this.GetComponent<Rigidbody>().velocity;
-            // 加一个向上的速度
-            this.GetComponent<Rigidbody>().velocity = new Vector3(vel2.x, 5, vel2.z);
-		};
+
+        if(GameManager._intance.GameStatus == GameManager.GAMESTATUS_PLAYING){
+
+			//control jump
+			if (Input.GetMouseButton(0))
+			{//lett mause bt down
+				Vector3 vel2 = this.GetComponent<Rigidbody>().velocity;
+				// 加一个向上的速度
+				this.GetComponent<Rigidbody>().velocity = new Vector3(vel2.x, 5, vel2.z);
+			};
+
+        }
+
+       
 
 
 
 
 	}
 
+
+    public void getLife(){
+        //重力
+        this.GetComponent<Rigidbody>().useGravity = true;
+		//速度
+		  this.GetComponent<Rigidbody>().velocity = new Vector3(2, 0, 0);
+
+	}
 
 
 
--- FlappyBird/Assets/Scripts/GameManager.cs
+++ FlappyBird/Assets/Scripts/GameManager.cs
@@ -2,16 +2,44 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+    //define  status of game 
+    public static int GAMESTATUS_MENU = 0;//菜单状态
+    public static int GAMESTATUS_PLAYING = 1;//游戏状态
+    public static int GAMESTATUS_END = 2;//游戏结束
+
+    public int GameStatus = GAMESTATUS_MENU;
     //first bg
     public Transform firstBg;
     // score
     public int totalScore = 0;
     //单例对象
     public static GameManager _intance;
+    //brid 
+    private GameObject brid;
     //启动
     private void Awake()
     {
         _intance = this;
+		brid = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    private void Update()
+    {
+        if(GameStatus == GAMESTATUS_MENU){
+            if(Input.GetMouseButtonDown(0)){
+                GameStatus = GAMESTATUS_PLAYING;
+                //添加重力
+                brid.SendMessage("getLife");
+                // 天津爱
+
+            }
+
+
+        }
+
+
+        
     }
 
 
