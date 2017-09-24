5b58d6f2b6254f170597d8b162808b9f23f764ed
Cstzhang <835066041@qq.com>
2017-09-24 19:47:00 +0800

    循环生成游戏场景

---
+++ FlappyBird/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    //first bg
+    public Transform firstBg;
+    //单例对象
+    public static GameManager _intance;
+    //启动
+    private void Awake()
+    {
+        _intance = this;
+    }
+
+
+}
---
+++ FlappyBird/Assets/Scripts/MoveTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTrigger : MonoBehaviour {
+    public Transform currentBg;
+    public PipeController pipe1;
+	public PipeController pipe2;
+
+    //当一个碰撞去进入碰撞去
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player"){
+            //Move th bg to the first transform
+            //1 get the first transtorm
+            Transform firstBg =   GameManager._intance.firstBg;
+            //2 move 
+            currentBg.position = new Vector3(firstBg.position.x + 10, currentBg.position.y, currentBg.position.z);
+            //3 update firstBg
+            GameManager._intance.firstBg = currentBg;
+            //4 new position for pipe
+            pipe1.RandomGeneratePosition();
+            pipe2.RandomGeneratePosition();
+        }
+    }
+}
