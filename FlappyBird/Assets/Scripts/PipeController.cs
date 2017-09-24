058c4334febc9daf5cb5dae7e4e9d953b99bd271
Cstzhang <835066041@qq.com>
2017-09-24 11:46:46 +0800

    管道随机

---
+++ FlappyBird/Assets/Scripts/PipeController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipeController : MonoBehaviour {
+     void Start()
+    { 
+        RandomGeneratePosition();  
+    }
+
+
+
+    // 生成随机数用来控制管道位置
+    public void RandomGeneratePosition(){
+        //random a number
+        float pos_y = Random.Range(-0.4f, -0.1f);
+        this.transform.localPosition = new Vector3(this.transform.localPosition.x, pos_y, this.transform.localPosition.z);
+
+
+
+    }
+
+ 
+
+
+
+
+}
